ed31a1c33755ad8d0c25e42d229cd5cb0a54a1f1
vijkr <vijkrbabu@gmail.com>
2024-02-19 22:02:45 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  project     = "thematic-carver-414815"
+  #credentials = file("tcb-project-371706-b114ce01e529.json")
+  region      = "us-east1"
+
+}
+
+resource "google_compute_network" "vpc1" {
+  name                    = "my-custom-network-1"
+  auto_create_subnetworks = "false"
+
+}
+
+resource "google_compute_subnetwork" "my-custom-subnet1" {
+  name          = "my-custom-subnet-1"
+  ip_cidr_range = "10.255.196.0/24"
+  network       = google_compute_network.vpc1.name
+  region        = "us-east1"
+}
+
+#### test file to push
